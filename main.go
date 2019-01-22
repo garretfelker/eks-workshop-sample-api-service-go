9f84558c089e0f97a218784d80953edd55d14c6e
garretfelker <10730149+garretfelker@users.noreply.github.com>
2019-01-22 14:09:03 -0600

    Update main.go

--- main.go
+++ main.go
@@ -33,7 +33,7 @@ func main() {
 
 		io.WriteString(w, string(out))
 
-		fmt.Println("Hello world - the log message")
+		fmt.Println("Hello world - the log message; new build!")
 	})
 	http.ListenAndServe(":8080", nil)
 }
